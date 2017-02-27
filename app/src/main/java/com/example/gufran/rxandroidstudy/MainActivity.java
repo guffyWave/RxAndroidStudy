12cfae15a46849385beab0a29e82670502fadc12
Gufran Khurshid <gufran.khurshid@fastfox.com>
2017-02-27 20:15:00 +0530

    Added Observer nd Observable

--- app/src/main/java/com/example/gufran/rxandroidstudy/MainActivity.java
+++ app/src/main/java/com/example/gufran/rxandroidstudy/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.gufran.rxandroidstudy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscription;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void clickMe(View view) {
+        Observable<String> observable = Observable.just("Hello RxGufran");
+
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onCompleted() {
+                Toast.makeText(MainActivity.this, "onCompleted ", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Toast.makeText(MainActivity.this, "Error ", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNext(String s) {
+                Toast.makeText(MainActivity.this, "Hey " + s, Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        Subscription mySubscription = observable.subscribe(observer);
+
+    }
+
+
 }
